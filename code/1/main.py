a078771834c0a45ae74cb51bf64875c956f422cc
Deepak <luckyme_deepak@yahoo.co.in>
2016-08-24 00:24:13 -0400

    edited and added screenshots

--- code/1/main.py
+++ code/1/main.py
@@ -1,11 +1,6 @@
-from utest import ok, oks
 import who1
 import who2
 import who3
-
-@ok
-def _ok3():
-  "New passing test case"
-  assert 2==2, "equality failure"
+from utest import ok, oks
 
 oks()
--- code/1/who1.py
+++ code/1/who1.py
@@ -1,1 +1,5 @@
-
+from utest import ok
+@ok
+def _ok3():
+  "New passing test case"
+  assert 2==2, "equality failure"

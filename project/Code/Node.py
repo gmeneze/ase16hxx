ba213c0d71de9d8625c3591074aabca802c1f9d8
Deepak Nair <dnair@ncsu.edu>
2016-11-05 21:52:59 +0000

    created Node

---
+++ project/Code/Node.py
@@ -0,0 +1,9 @@
+class Node(object):
+    id = None
+    xcordinate = None
+    ycordinate = None
+    quantity = None
+    demand = None
+    earliestReach = None
+    latestReach = None
+    duration = None

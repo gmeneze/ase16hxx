5ec8c30ed3e4e917a3d84302c2692234a46c9555
Swetha Murali <smurali8@ncsu.edu>
2016-11-08 15:15:03 -0500

    Graphics Update with Time

--- project/Code/Drawer.py
+++ project/Code/Drawer.py
@@ -1,4 +1,5 @@
 from Graphics import *
+import time
 class Drawer(object):
     def __init__(self):
         self.window_height = 700
@@ -19,15 +20,23 @@ class Drawer(object):
             circle2 = Circle(point2, 5)
             circle2.setFill("red")
             circle2.draw(win)
-	        #circle1 = Circle(point1,5)
-    	    #circle1.setFill("yellow")
-    	    #circle1.draw(win)
-	        #circle2 = Circle(point2,5)
-            #circle2.setFill("yellow")
-            #circle2.draw(win)
-            line1 = Line(point1,point2) 
+            point1_txt = Point(self.nodelist[route[i]].xcordinate-15, self.nodelist[route[i]].ycordinate+15)
+            point2_txt = Point(self.nodelist[route[i + 1]].xcordinate-15, self.nodelist[route[i + 1]].ycordinate+15)
+            msg1 = Text(point1_txt,self.route[i])
+            msg1.draw(win)
+            msg2 = Text(point2_txt, self.route[i+1])
+            msg2.draw(win)
+        time.sleep(3)
+        message = Text(Point(150,10), self.route)
+        message.draw(win)
+        time.sleep(3)
+        for i in range(route.__len__()-1):
+            point1 = Point(self.nodelist[route[i]].xcordinate,self.nodelist[route[i]].ycordinate)
+            point2 = Point(self.nodelist[route[i+1]].xcordinate,self.nodelist[route[i+1]].ycordinate)
+            line1 = Line(point1,point2)
             line1.setWidth(3)
             line1.draw(win)
+            time.sleep(2)
         win.getMouse()
         win.close()
         
